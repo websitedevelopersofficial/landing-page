ffd54b5c918ef0a1ec6c56f699aa5629ced9433f
websitedevelopersofficial <112344051+websitedevelopersofficial@users.noreply.github.com>
2024-02-09 20:43:48 +0530

    Update shabashguruji.js

--- shabashguruji.js
+++ shabashguruji.js
@@ -1,4 +1,38 @@
-var authorized = ["pagalbeats.blogspot.com", "probusiness-websitedevelopers.blogspot.com", "www.studentvisa24.com"];
+var authorized = [
+    "kinwatliveblog.blogspot.com",
+    "lokbharatihindi.blogspot.com",
+    "RSGiikwad.blogspot.com",
+    "dnyaneshwarpatil.blogspot.com",
+    "shabbasguru.blogspot.com",
+    "vidyaughormadekute.blogspot.com",
+    "httseduexpert.blogspot.com",
+    "kondbadhumale.blogspot.com",
+    "lgbadne78.blogspot.com",
+    "shivajigangle.blogspot.com",
+    "vdtsmprs.blogspot.com",
+    "sanjaysawalesir.blogspot.com",
+    "nivruttigode777.blogspot.com",
+    "languageedgeblog.blogspot.com",
+    "vinodkumarmane358.blogspot.com",
+    "vdtsmprs.blogspot.com",
+    "anjalikhandzode.blogspot.com",
+    "diamondeducare.blogspot.com",
+    "rsgiikwad.blogspot.com",
+    "adityarajinfotech.blogspot.com",
+    "httseduexpert.blogspot.com",
+    "yashuikey.blogspot.com",
+    "vmguruji.blogspot.com",
+    "diwakarbaviskar.blogspot.com",
+    "rajkumarchanne26.blogspot.com",
+    "blogofenglishforjuniorcollege.blogspot.com",
+    "vaishusamudre.blogspot.com",
+    "palkhedkar69.blogspot.com",
+    "anjumam123.blogspot.com",
+    "Aapalishala.blogspot.com",
+    "rajendrabedwal.blogspot.com",
+    "pagalbeats.blogspot.com",
+    "mungekarsatish.blogspot.com"
+];
 var RequestUrl = location.hostname;
 if($.inArray(RequestUrl, authorized) > -1) {
 $(".main").attr('style', 'visibility: visible;');
